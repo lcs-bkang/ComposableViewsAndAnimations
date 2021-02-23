1613dde9744e1a89794f23e64e753a649d9a4c52
Bradley Kang <bkang@lcs.on.ca>
2021-02-23 15:08:17 -0500

    Exercise one is weird?

--- ComposableViewsAndAnimations/Views/Exercises/ExerciseOneView.swift
+++ ComposableViewsAndAnimations/Views/Exercises/ExerciseOneView.swift
@@ -14,15 +14,18 @@ struct ExerciseOneView: View {
     
     // Controls whether this view is showing or not
     @Binding var showThisView: Bool
-        
+    
     // Controls what typeface the text is shown in
     @State private var typeFace: String = "Helvetica-Neue"
-
+    
     // Whether to apply the animation
-    @State private var useAnimation = false
+    @State private var useAnimation = true
+    
+    // Controls the hue of the text
+    @State private var hue: Color = .red
 
     // MARK: Computed properties
-
+    
     // List all fonts available
     // NOTE: This is a very useful gist...
     //       https://gist.github.com/kristopherjohnson/c825cb97b1ad1fe0bc13d709986d0763
@@ -33,37 +36,40 @@ struct ExerciseOneView: View {
         }
         return names.sorted()
     }()
-
+    
     var body: some View {
         
         NavigationView {
             
             VStack {
-                
                 Text(typeFace)
                     .font(.custom(typeFace, size: 30.0))
-                
-            }
-            .navigationTitle("Exercise 1")
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Button("Done") {
-                        hideView()
+                    .onTapGesture {
+                        hue = Color(hue: Double.random(in: 1...360) / 360.0,
+                                    saturation: 0.8,
+                                    brightness: 0.8)
+                            
                     }
+
+            }
+            .animation(useAnimation ? .interpolatingSpring(mass: 1.0, stiffness: 1.0, damping: 0.75, initialVelocity: 10) : .none)
+        }
+        .navigationTitle("Exercise 1")
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button("Done") {
+                    hideView()
                 }
             }
-
         }
-        
     }
     
     // MARK: Functions
-    
+
     // Makes this view go away
     func hideView() {
         showThisView = false
     }
-    
 }
 
 struct ExerciseOneView_Previews: PreviewProvider {
